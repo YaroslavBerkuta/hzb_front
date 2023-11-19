59e95ba2c2490987754da34fd0715dc2067fc013
YaroslavBerkuta <yaroslavberkuta@gmail.com>
2023-11-19 23:20:08 +0200

    update

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/pro/Documents/hzb/hzb_front/src/app/layout":["static/media/634216363f5c73c1-s.p.woff2","static/media/e1c529c04de64b40-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
