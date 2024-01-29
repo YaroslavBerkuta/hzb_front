e7bf8052889740ba292e7568f20cd3ce117c80be
YaroslavBerkuta <yaroslavberkuta@gmail.com>
2024-01-29 20:00:13 +0200

    change conatct block

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,50 +1,10 @@
-"use client";
-
-import { IMainFormData } from "@/shared/api/form";
-import { Breadcrumbs, ButtonWhite } from "@/shared/components";
-import { sendForm } from "@/shared/services/domain/mail.service";
-import React, { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
-import { Map } from "./distribution/components";
+import { Breadcrumbs } from "@/shared/components";
+import React from "react";
 
 import styles from "./index.module.scss";
-import map from "./distribution/index.module.scss";
-import { getTranslate } from "@/shared/helpers";
-import { distributorApi } from "@/shared/api/distributor";
 import Image from "next/image";
 
 export default function Contacts() {
-  const [key, setKey] = useState("va");
-  const [dataMap, setDataMap] = useState<any>(null);
-  const load = async () => {
-    try {
-      const { data } = await distributorApi.getDistributor(key);
-      setDataMap(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    load();
-  }, [key]);
-  const { t, i18n } = useTranslation();
-  const [data, setData] = useState<IMainFormData>({
-    name: "",
-    surname: "",
-    phone: "",
-    email: "",
-    comment: "",
-  });
-  const handleSubmit = async () => {
-    try {
-      await sendForm(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const onChange = (val: string, key: string) => {
-    setData((prev) => ({ ...prev, [key]: val }));
-  };
   return (
     <>
       <section>
@@ -53,130 +13,212 @@ export default function Contacts() {
       <section>
         <div className="container">
           <div className={styles.flex}>
-            <div>
+            <div className={styles.item}>
               <div className="sectionTitle">
                 <h2>Приймальня</h2>
               </div>
-              <ul className={styles.contact}>
+              <ul className={styles.contactWarapper}>
                 <li>
-                  <span>{t("shared.element.phone")}:</span>
-                  <p>(0382)644396 (0382)644358</p>
+                  <span>Телефон:</span>
+                  <div>
+                    <p>(0382)644396 </p>
+                    <p>(0382)644358</p>
+                  </div>
                 </li>
                 <li>
-                  <span>{t("shared.element.email")}:</span>
-                  <p>xmbeton@gmail.com</p>
-                </li>
-              </ul>
-              <ul className={styles.contact}>
-                <div className="sectionTitle">
-                  <h2>Бухгалтерія</h2>
-                </div>
-                <li>
-                  <span>{t("shared.element.phone")}:</span>
-                  <p>+380975401087 +380974890838 (0382)644369 </p>
-                </li>
-                <li>
-                  <span>{t("shared.element.email")}:</span>
-                  <p>xmbeton@gmail.com</p>
-                </li>
-              </ul>
-              <ul className={styles.contact}>
-                <div className="sectionTitle">
-                  <h2>ВІДДІЛ ПОСТАЧАННЯ</h2>
-                </div>
-                <li>
-                  <span>{t("shared.element.phone")}:</span>
-                  <p>+380964529572  +380977013673 (0382)645300 </p>
-                </li>
-                <li>
-                  <span>{t("shared.element.email")}:</span>
-                  <p>dimad9441@gmail.com</p>
-                </li>
-              </ul>
-              <ul className={styles.contact}>
-                <div className="sectionTitle">
-                  <h2>Випробувальна лабораторія</h2>
-                </div>
-                <li>
-                  <span>{t("shared.element.phone")}:</span>
-                  <p>+380978639352 (0382)643205</p>
-                </li>
-                <li>
-                  <span>{t("shared.element.email")}:</span>
-                  <p>labhzb@ukr.net</p>
-                </li>
-              </ul>
-              <ul className={styles.contact}>
-                <div className="sectionTitle">
-                  <h2>Виробничий відділ</h2>
-                </div>
-                <li>
-                  <span>{t("shared.element.phone")}:</span>
-                  <p>+380673113632 (0382)644355</p>
-                </li>
-                <li>
-                  <span>{t("shared.element.email")}:</span>
-                  <p>smolicigor1@gmail.com</p>
+                  <span>E-mail:</span>
+                  <div>
+                    <p className={styles.email}>xmbeton@gmail.com</p>
+                  </div>
                 </li>
               </ul>
             </div>
-            <div>
+            <div className={styles.item}>
               <div className="sectionTitle">
                 <h2>Відділ збуту</h2>
               </div>
-              <ul className={styles.contact}>
-                <p>ЗАЛІЗОБЕТОНІ ВИРОБИ</p>
+              <ul className={styles.contactWarapper}>
+                <div className="sectionTitle">
+                  <h2 style={{ fontWeight: "400" }}>ЗАЛІЗОБЕТОНІ ВИРОБИ</h2>
+                </div>
                 <li>
-                  <span>{t("shared.element.phone")}:</span>
-                  <p>+380969176074 +380988671678 (0382)643025 (0382)643603</p>
+                  <span>Телефон:</span>
+                  <div>
+                    <p>+380969176074</p>
+                    <p>+380988671678</p>
+                    <p>(0382)643025</p>
+                    <p>(0382)643603</p>
+                  </div>
                 </li>
                 <li>
-                  <span>{t("shared.element.email")}:</span>
-                  <p>skhzb.zbut@gmail.com</p>
-                </li>
-              </ul>
-              <ul className={styles.contact}>
-                <p>БЕТОНИ ТА РОЗЧИНИ</p>
-                <li>
-                  <span>{t("shared.element.phone")}:</span>
-                  <p>+380967875437 +380985060240 +380673811183 (0382)645262 </p>
-                </li>
-                <li>
-                  <span>{t("shared.element.email")}:</span>
-                  <p>a-hzb@ukr.net</p>
+                  <span>E-mail:</span>
+                  <div>
+                    <p className={styles.email}>skhzb.zbut@gmail.com</p>
+                  </div>
                 </li>
               </ul>
-              <ul className={styles.contact}>
-                <p>ВИРОБИ З ДЕРЕВИНИ</p>
+            </div>
+            <div className={styles.item}>
+              <div className="sectionTitle">
+                <h2>Бухгалтерія</h2>
+              </div>
+              <ul className={styles.contactWarapper}>
                 <li>
-                  <span>{t("shared.element.phone")}:</span>
-                  <p>+380987377278 (0382)645136 </p>
+                  <span>Телефон:</span>
+                  <div>
+                    <p>+380975401087</p>
+                    <p>+380974890838</p>
+                    <p>(0382)644369</p>
+                  </div>
                 </li>
                 <li>
-                  <span>{t("shared.element.email")}:</span>
-                  <p>zbuthzb.st@gmail.com</p>
+                  <span>E-mail:</span>
+                  <div>
+                    <p className={styles.email}>xmbeton@gmail.com</p>
+                  </div>
                 </li>
               </ul>
-              <ul className={styles.contact}>
-                <p>ЗАСТУПНИК КЕРІВНИКА ВІДДІЛУ ЗБУТУ</p>
+            </div>
+            <div className={styles.item}>
+              <div className="sectionTitle">
+                <h2 style={{ fontWeight: "400" }}>ЗАЛІЗОБЕТОНІ ВИРОБИ</h2>
+              </div>
+              <ul className={styles.contactWarapper}>
                 <li>
-                  <span>{t("shared.element.phone")}:</span>
-                  <p>+380674950275 </p>
+                  <span>Телефон:</span>
+                  <div>
+                    <p>+380967875437 </p>
+                    <p>+380985060240</p>
+                    <p>+380673811183</p>
+                    <p>(0382)645262</p>
+                  </div>
                 </li>
                 <li>
-                  <span>{t("shared.element.email")}:</span>
-                  <p>v-hzb@ukr.net</p>
+                  <span>E-mail:</span>
+                  <div>
+                    <p className={styles.email}>a-hzb@ukr.net</p>
+                  </div>
                 </li>
               </ul>
-              <ul className={styles.contact}>
-                <p>КЕРІВНИК ВІДДІЛУ ЗБУТУ</p>
+            </div>
+            <div className={styles.item}>
+              <div className="sectionTitle">
+                <h2>ВІДДІЛ ПОСТАЧАННЯ</h2>
+              </div>
+              <ul className={styles.contactWarapper}>
                 <li>
-                  <span>{t("shared.element.phone")}:</span>
-                  <p>+380672772202 </p>
+                  <span>Телефон:</span>
+                  <div>
+                    <p>+380964529572 </p>
+                    <p>+380977013673</p>
+                    <p>(0382)645300</p>
+                  </div>
                 </li>
                 <li>
-                  <span>{t("shared.element.email")}:</span>
-                  <p>s_hzb@ukr.net</p>
+                  <span>E-mail:</span>
+                  <div>
+                    <p className={styles.email}>dimad9441@gmail.com</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className={styles.item}>
+              <div className="sectionTitle">
+                <h2 style={{ fontWeight: "400" }}>ВИРОБИ З ДЕРЕВИНИ</h2>
+              </div>
+              <ul className={styles.contactWarapper}>
+                <li>
+                  <span>Телефон:</span>
+                  <div>
+                    <p>+380987377278</p>
+                    <p>(0382)645136</p>
+                  </div>
+                </li>
+                <li>
+                  <span>E-mail:</span>
+                  <div>
+                    <p className={styles.email}>zbuthzb.st@gmail.com</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className={styles.item}>
+              <div className="sectionTitle">
+                <h2>Випробувальна лабораторія</h2>
+              </div>
+              <ul className={styles.contactWarapper}>
+                <li>
+                  <span>Телефон:</span>
+                  <div>
+                    <p>+380978639352</p>
+                    <p>(0382)643205</p>
+                  </div>
+                </li>
+                <li>
+                  <span>E-mail:</span>
+                  <div>
+                    <p className={styles.email}>labhzb@ukr.net</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className={styles.item}>
+              <div className="sectionTitle">
+                <h2 style={{ fontWeight: "400" }}>
+                  ЗАСТУПНИК КЕРІВНИКА ВІДДІЛУ ЗБУТУ
+                </h2>
+              </div>
+              <ul className={styles.contactWarapper}>
+                <li>
+                  <span>Телефон:</span>
+                  <div>
+                    <p>+380674950275</p>
+                  </div>
+                </li>
+                <li>
+                  <span>E-mail:</span>
+                  <div>
+                    <p className={styles.email}>v-hzb@ukr.net</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className={styles.item}>
+              <div className="sectionTitle">
+                <h2>Виробничий відділ</h2>
+              </div>
+              <ul className={styles.contactWarapper}>
+                <li>
+                  <span>Телефон:</span>
+                  <div>
+                    <p>+380673113632</p>
+                    <p>(0382)644355</p>
+                  </div>
+                </li>
+                <li>
+                  <span>E-mail:</span>
+                  <div>
+                    <p className={styles.email}>smolicigor1@gmail.com</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className={styles.item}>
+              <div className="sectionTitle">
+                <h2 style={{ fontWeight: "400" }}>КЕРІВНИК ВІДДІЛУ ЗБУТУ</h2>
+              </div>
+              <ul className={styles.contactWarapper}>
+                <li>
+                  <span>Телефон:</span>
+                  <div>
+                    <p>+380672772202</p>
+                  </div>
+                </li>
+                <li>
+                  <span>E-mail:</span>
+                  <div>
+                    <p className={styles.email}>s_hzb@ukr.net</p>
+                  </div>
                 </li>
               </ul>
             </div>
